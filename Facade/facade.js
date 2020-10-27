2d2fd961152b49039562a22a3fbc29ae3e3429a3
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-10-27 19:47:52 +0300

    Facade

---
+++ Facade/facade.js
@@ -0,0 +1,53 @@
+class Conveyor {
+    setBody() { console.log('Body set!') }
+
+    getEngine() { console.log('Dismantle engine!') }
+
+    setEngine() { console.log('Engine set!') }
+
+    getInterior() { console.log('Interior updated!') }
+
+    setInterior() { console.log('Added interior!') }
+
+    setExterior() { console.log('Exterior added!') }
+
+    setWheels() { console.log('Wheels!') }
+
+    addElectronics() { console.log('Added electronics!') }
+
+    paint() { console.log('Car painted!') }
+}
+
+class ConveyorFacade {
+    constructor(car) {
+        this.car = car;
+    }
+
+    assembleCar() {
+        this.car.setBody();
+        this.car.setEngine();
+        this.car.setInterior();
+        this.car.setExterior();
+        this.car.setWheels();
+        this.car.addElectronics();
+        this.car.paint();
+    }
+
+    changeEngine() {
+        this.car.getEngine();
+        this.car.setEngine();
+    }
+
+    changeInterior() {
+        this.car.getInterior();
+        this.car.setInterior();
+    }
+}
+
+const conveyor = new ConveyorFacade(new Conveyor());
+
+let car = conveyor.assembleCar();
+car = conveyor.changeEngine();
+car = conveyor.changeInterior();
+
+console.log(car);
